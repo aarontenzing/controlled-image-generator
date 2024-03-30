ebfbac4462513c33bcc4f2c93ce17197fc1aee62
aarontenzing <aarontenzing@gmail.com>
2024-03-30 13:20:17 +0100

    prompt generator

---
+++ prompt.py
@@ -0,0 +1,56 @@
+import csv
+from random import choices
+
+with open('descriptions\\details.csv', newline='') as csvfile:
+    reader = csv.reader(csvfile)
+
+    appearance = []
+    person = []
+    details1_box = []
+    details2_box = []
+    company = []
+    background = []
+    weather = []
+
+    # Parse the CSV data into corresponding lists
+    current_list = None
+    for line in reader:
+        if line:
+            line = line[0]
+            # print(line)
+            if line.startswith('#'):
+                current_list = line.strip('#').strip()
+            else:
+                if current_list == 'appearance':
+                    appearance.append(line.strip())
+                elif current_list == 'person':
+                    person.append(line.strip())
+                elif current_list == 'details1_box':
+                    details1_box.append(line.strip())
+                elif current_list == 'details2_box':
+                    details2_box.append(line.strip())
+                elif current_list == 'company':
+                    company.append(line.strip())
+                elif current_list == 'background':
+                    background.append(line.strip())
+                elif current_list == 'weather':
+                    weather.append(line.strip())
+
+    # Print the filled lists
+    # print("Appearance:", appearance)
+    # print("Person:", person)
+    # print("Details1_box:", details1_box)
+    # print("Details2_box:", details2_box)
+    # print("Company:", company)
+    # print("Background:", background)
+    # print("Weather:", weather)
+    
+    
+    def prompt_generator():
+        personType = choices(person)[0]
+        pronoun = 'his' if personType in ['boy', 'man'] else 'her'
+        pronoun = choices(['his', 'her'])[0] if personType == 'child' else pronoun
+        return f"A mid shot view of {choices(appearance)[0]} {personType} holding a {choices(details1_box)[0]} cardboard box {choices(details2_box)[0]}, parcel from {choices(company)[0]} webshop in {pronoun} hands, standing in front of a {choices(background)[0]} background, the weather is {choices(weather)[0]}, high photorealistic quality."
+
+if __name__ == '__main__':
+    print(prompt_generator())
